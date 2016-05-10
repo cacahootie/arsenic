e06bced8cd6e3b855d022d3eb98a73ea397b5cde
Brad Smith <brad.alan.smith@gmail.com>
2016-05-10 11:08:34 -0700

    echo!!

--- main.go
+++ main.go
@@ -1,42 +1,52 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"github.com/parnurzeal/gorequest"
 )
 
 type requestOptions struct {
-	url, method string
+	Url string `json:url`
+	Method string `json:method`
+	Query string `json:query`
 }
 
-func RequestOptions (destUrl string) (retval requestOptions) {
-	retval = requestOptions{"http://www.reddit.com/top.json", "GET"}
+func RequestOptions(url, method, query string) (retval requestOptions) {
+	retval = requestOptions{url, method, query}
 	return
 }
 
 var request = gorequest.New()
 
-func DoRequest (opts requestOptions) (data interface{}) {
-	resp, body, errs := request.CustomMethod(opts.method, opts.url).
+func DoRequest(opts requestOptions) (data interface{}, err error) {
+	r := request.CustomMethod(opts.Method, opts.Url).
 		SetDebug(true).
-		Set("User-Agent","Super-spiffy golang useragent /u/cacahootie").
-	End()
+		Set("User-Agent", "Super-spiffy golang useragent /u/cacahootie")
+	if opts.Query != "" {
+		r.Query(opts.Query)
+	}
+	resp, body, errs :=	r.End()
 	data = body
 	if errs != nil {
 		fmt.Println(errs)
-		return
 	} else if resp.Status != "200 OK" {
 		fmt.Println(resp.Status)
 		fmt.Println("non 200 status code")
-		return
-	} else {
-		fmt.Print(body)
-		return
 	}
+	return
 }
 
 func main() {
-	DoRequest(
-		RequestOptions("http://www.reddit.com/top.json")
-	)
+	var opts requestOptions
+	raw, err := ioutil.ReadFile("./demo/config.json")
+    if err != nil {
+        fmt.Println(err.Error())
+        return
+    }
+
+    json.Unmarshal(raw, &opts)
+	//opts = RequestOptions("http://localhost:8080", "GET", "monkey=flan")
+	DoRequest(opts)
 }
